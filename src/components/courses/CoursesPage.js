2c7763d3179f553641b4d61c6c561c980664d92b
kip <kkipngenokoech22@gmail.com>
2023-09-17 10:04:45 +0300

    setting up redux flow nxt

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,7 +1,33 @@
+import React from "react";
+import { Field, Form, Formik } from "formik";
+
 export default function CoursesPage() {
-    return (
-        <div>
-            <h2>Courses</h2>
-        </div>
-    )
+  const courses = { title: "", lecturer: "", room: "" };
+
+  function handleSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <div>
+      <h2>Courses</h2>
+      <Formik initialValues={courses} onSubmit={handleSubmit}>
+        {() => (
+          <Form>
+            <h3>Add course</h3>
+            <div>
+              <label htmlFor="title">Title:</label>
+              <Field
+                type="text"
+                id="title"
+                name="title"
+                placeholder="Course Title"
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
